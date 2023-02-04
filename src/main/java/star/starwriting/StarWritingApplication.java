e59c20c4c7415605871908f90743f90d88ab61ed
unwind <ysh038@naver.com>
2023-02-04 16:07:48 +0900

    google OAuth2.0 token

--- src/main/java/star/starwriting/StarWritingApplication.java
+++ src/main/java/star/starwriting/StarWritingApplication.java
@@ -3,10 +3,13 @@ package star.starwriting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StarWritingApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(StarWritingApplication.class, args);
 	}
 
--- src/main/java/star/starwriting/controller/MemberController.java
+++ src/main/java/star/starwriting/controller/MemberController.java
@@ -1,16 +1,16 @@
 package star.starwriting.controller;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import star.starwriting.domain.Member;
 import star.starwriting.dto.MemberRequestDto;
 import star.starwriting.dto.MemberResponseDto;
 import star.starwriting.service.MemberService;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 public class MemberController {
@@ -32,9 +32,52 @@ public class MemberController {
     }
 
     @GetMapping("/api/members/google")
-    public Map<String, Object> currentMember(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
-        return oAuth2AuthenticationToken.getPrincipal().getAttributes();
+    public Map<String, Object> currentMember(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        Map<String, Object> data = oAuth2AuthenticationToken.getPrincipal().getAttributes();
+
+
+        try {
+            // [UTC DATE 포맷 형식 지정 실시]
+            String formatTimeZone = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
+            // [UTC 형식 Date 값 정의 실시]
+            //String utcDateValue="2022-11-08T05:25:00.000Z";
+            String expiredDate = data.get("exp").toString();
+            String result = expiredDate.substring(0,expiredDate.length()-1);
+            result = result+ ".000Z";
+
+            // [UTC 값을 포맷 형식 지정해 Date 객체로 선언]
+            Date date = new SimpleDateFormat(formatTimeZone).parse(result);
+
+            // [Date to Calendar 변환]
+            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            calendar.setTime(date);
+
+            // [한국형 시간은 UTC 시간보다 9시간 앞선다]
+            calendar.add(Calendar.HOUR, 9);
+
+
+            // [한국형 시간으로 변경된 값 저장 실시]
+            SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String koreaDate = sdformat.format(calendar.getTime());
+            System.out.println("로그인 만료기한: " + koreaDate);
+        }
+        catch (Exception e){
+            System.out.println("에러: "+ e);
+        }
+        return data;
     }
+
+//    @GetMapping("/api/test/google")
+//    public String testPeopleAPI(){
+//        RestTemplate restTemplate = new RestTemplate();
+//        String accessToken = "AIzaSyCDIPKXqXD5DjwlclMTT0CF2QJy68lhm9Y";
+//        String url = "https://people.googleapis.com/v1/people/me?personFields=emailAddresses,names&access_token=" + accessToken;
+//        System.out.println(url);
+//        String response = restTemplate.getForObject(url, String.class);
+//        System.out.println(response);
+//        return response;
+//    }
 
     @PostMapping("/api/members")
     public Long saveMember(@RequestBody MemberRequestDto requestDto) {
