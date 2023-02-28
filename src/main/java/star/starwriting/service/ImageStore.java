91d29e89c03537e5e37bb5164be1cf0a289e797c
[username] <ysh038@naver.com>
2023-02-28 13:27:01 +0900

    기본 프로필 이미지 설정

--- src/main/java/star/starwriting/service/ImageStore.java
+++ src/main/java/star/starwriting/service/ImageStore.java
@@ -33,17 +33,36 @@ public class ImageStore {
     public MemberProfileImage storeImage(MultipartFile file, Member member) throws IOException {
 
         // 프로필 이미지 저장
-        if(file == null) {
+
+        // MultipartFile은 null이 아니라 isEmpty로 null체크
+        // 파라미터로 받아온 file이 null일 경우 프로필이미지를 기본 이미지로 설정
+        if(file.isEmpty()) {
+            String originalFilename = "basicProfile.png";
+            String storeFileName = "basicProfile.png";
+            String fileUrl = "src/main/resources/static/img/"+storeFileName;
+
+            MemberProfileImageDto profileImageDto = new MemberProfileImageDto(
+                    originalFilename,
+                    storeFileName,
+                    fileUrl
+            );
+
+            MemberProfileImage profileImage = profileImageDto.toEntity();
+            memberProfileImageRepository.save(profileImage);
+            member.setProfileImage(profileImage); // 회원가입한 멤버의 profileImage에 저장한 이미지를 할당
+
             return null;
         }
 
+        // file이 null이 아니라면 아래로
         String originalFilename = file.getOriginalFilename();
         // 작성자가 업로드한 파일명 -> 서버 내부에서 관리하는 파일명
         // 파일명을 중복되지 않게끔 UUID로 정하고 ".확장자"는 그대로
         String storeFileName = UUID.randomUUID() + "." + extractExt(originalFilename);
-//        String storeFileName = file.getOriginalFilename();
+//      String storeFileName = file.getOriginalFilename();
         String fileUrl = "src/main/resources/static/img/"+storeFileName;
         String fullPath = "C:\\Users\\82109\\Desktop\\웹\\StarWriting\\src\\main\\resources\\static\\img\\" + storeFileName;
+
 
         // 파일을 저장하는 부분 -> 파일경로 + storeFilename 에 저장
         file.transferTo(new File(fullPath));
