1184b1aa06e8c4a6a2e85592ebdfdfcadc2c6765
GgeaBu <h2sorginal@gmail.com>
2023-02-28 22:22:55 +0900

    회원가입시 맴버별 폴더 생성

--- src/main/java/star/starwriting/service/DirManager.java
+++ src/main/java/star/starwriting/service/DirManager.java
@@ -9,6 +9,9 @@ public class DirManager {
 
     private final String rootPath = System.getProperty("user.dir");
     private final String membersDirPath = rootPath + PathSeperator("/src/main/resources/static/members/");
+    /**
+     * 회원가입시 맴버별 폴더 생성
+     * */
     public void makeMemberDir(Long memberId) {
         String memberDirPath = membersDirPath + memberId.toString();
         File memberDir = new File(memberDirPath);
